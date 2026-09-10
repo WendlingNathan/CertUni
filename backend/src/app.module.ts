738b03451fd151d83a3afcd840b094cafe283d08
WendlingNathan <natritwen@gmail.com>
2026-09-10 19:56:25 -0300

    Adição: Criação dos módulos e controllers do NestJS
    Estrutura dos módulos e controllers por CLI do NestJS criada

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
