c11242d04c9fd2ef4a28c37c1230103c721ff2a0
WendlingNathan <natritwen@gmail.com>
2026-09-10 20:36:08 -0300

    Ajuste: Adição do JWT no código
    Configuração do JWT para tratar da criptografia das senhas

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET || 'super-secret-key', // definir no .env
+      signOptions: { expiresIn: '1d' }, // token expira em 1 dia
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
